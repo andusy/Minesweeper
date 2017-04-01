6d93941962f85bd0d8a343c9051376a4d7c6ffa7
15ay3 <andus401@gmail.com>
2017-04-01 05:03:23 -0400

    added algorithm for numbering

--- scripts.js
+++ scripts.js
@@ -22,14 +22,14 @@ for (i = 0; i < x; i++){ //Creating 2d array
 var flip = new Array(x); //The board that contains the tiles
 
 for (i = 0; i < x; i++){ //Creating 2d array
-	flip = new Array(y);
+	flip[i] = new Array(y);
 }
 
 //Variable to representing the tiles with numbers on them
 var tile = new Array(x); //The board that contains the tiles
 
 for (i = 0; i < x; i++){ //Creating 2d array
-	tile = new Array(y);
+	tile[i] = new Array(y);
 }
 
 function startGame(){
@@ -38,15 +38,28 @@ function startGame(){
 
 	initBoard(play_field, flip, tile); //Initialization of the board
 
+	for (i = 0; i < x; i++){
+		for (j = 0; j < y; j++){
+			document.write(play_field[i][j] + " ");
+		}
+		document.write("<br>");
+	}
+
+	for (i = 0; i < x; i++){
+		for (j = 0; j < y; j++){
+			document.write(tile[i][j] + " ");
+		}
+		document.write("<br>");
+	}
+
 	//setInterval(function(){ updateBoard(board) }, 1000/30); //Updates the heading (Time/Number of mines)
 	setInterval(function(){ updateBoard(board) }, 1000/30); //Updates the board tiles
 }
 
 function updateBoard(b){
 	//Draws canvas
-	b.beginPath();
-	b.arc(100,50,40,0,2*Math.PI);
-	b.stroke();
+	b.fillStyle="#3D7E85";
+	b.fillRect(0,0,600,80);
 }
 
 //Initializes the board to have 99 mines (true), every element is unflipped and assigns a number to each tile
@@ -57,7 +70,7 @@ function initBoard(field, status, num){
 	for (i = 0; i < x; i++){
 		for (j = 0; j < y; j++){
 			field[i][j] = EMPTY;
-			field[i][j] = NOTFLIPPED;
+			status[i][j] = NOTFLIPPED;
 		}
 	}
 
@@ -73,7 +86,7 @@ function initBoard(field, status, num){
 			ycount = 0;
 		}
 
-		if ( Math.random() >= 0.5 && field[xcount][ycount] == EMPTY){ //Checks the condition that the tile is to be filled and it is currently empty
+		if ( Math.random() * 10 >= 9 && field[xcount][ycount] == EMPTY){ //Checks the condition that the tile is to be filled and it is currently empty
 			field[xcount][ycount] = true;
 			num_mines--;
 		}
@@ -91,35 +104,94 @@ function initBoard(field, status, num){
 	.
 	x-1,0	...	x-1,y-1
 	*/
-
-	/*
+	
+	//Assigns numbers to the tiles representing the number of mines that each tile is touching
 	for (i = 0; i < x; i++){
-		num_mines_touching = 0;
 		for (j = 0; j < y; j++){
-			//Checking special cases
-			if (i == 0 && j == 0){ //Top left
-
-			} else if (i == 0 && j == y-1){ //Top right
-
-			} else if (i == 0){ //Top row
-
-			} else if (i == x-1 && j == 0){//Bottom Left
-
-			} else if (i == x-1 && j == y-1){//Bottom right
-
-			} else if (i == x-1 ){//Bottom row
-
-			} else if (y == 0){//Left Column
-
-			} else if (y == y-1){//Right Column
-
-			} else	{ //No special case
-
+			num_mines_touching = 0; //Reset count to 0
+				
+			if (field[i][j] == EMPTY){
+				//Checking special cases
+				if (i == 0 && j == 0){ //Top left
+					for (k = 0; k <= 1; k++){
+						for (m = 0; m <= 1;m++){
+							if (field[k][m] == FILLED){
+								num_mines_touching++;
+							}
+						}	
+					}
+				} else if (i == 0 && j == y-1){ //Top right
+					for (k = 0; k <= 1; k++){
+						for (m = j-1; m <= j;m++){
+							if (field[k][m] == FILLED){
+								num_mines_touching++;
+							}
+						}	
+					}
+				} else if (i == 0){ //Top row
+					for (k = 0; k <= 1; k++){
+						for (m = j-1; m <= j+1;m++){
+							if (field[k][m] == FILLED){
+								num_mines_touching++;
+							}
+						}	
+					}
+				} else if (i == x-1 && j == 0){//Bottom Left
+					for (k = i-1; k <= i; k++){
+						for (m = j; m <= j+1;m++){
+							if (field[k][m] == FILLED){
+								num_mines_touching++;
+							}
+						}	
+					}
+				} else if (i == x-1 && j == y-1){//Bottom right
+					for (k = i-1; k <= i; k++){
+						for (m = j-1; m <= j;m++){
+							if (field[k][m] == FILLED){
+								num_mines_touching++;
+							}
+						}	
+					}
+				} else if (i == x-1 ){//Bottom row
+					for (k = i-1; k <= i; k++){
+						for (m = j-1; m <= j+1;m++){
+							if (field[k][m] == FILLED){
+								num_mines_touching++;
+							}
+						}	
+					}
+				} else if (j == 0){//Left Column
+					for (k = i-1; k <= i+1; k++){
+						for (m = j; m <= j+1;m++){
+							if (field[k][m] == FILLED){
+								num_mines_touching++;
+							}
+						}	
+					}
+				} else if (j == y-1){//Right Column
+					for (k = i-1; k <= i+1; k++){
+						for (m = j-1; m <= j;m++){
+							if (field[k][m] == FILLED){
+								num_mines_touching++;
+							}
+						}	
+					}
+				} else	{ //No special case
+					for (k = i-1; k <= i+1; k++){
+						for (m = j-1; m <= j+1;m++){
+							if (field[k][m] == FILLED){
+								num_mines_touching++;
+							}
+						}	
+					}
+				}
+			} else {
+				num_mines_touching = -1; //-1 represents the tiles with a mine
 			}
-
+			num[i][j] = num_mines_touching;
 		}
-	}*/
-
+	}
+	
 }
 
 
